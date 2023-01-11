550c8d6b0bb3eeb52518a3c0c044d56ee9f10bd2
noahBana <nbana007@gmail.com>
2023-01-10 20:03:29 -0500

    fix spotlessapply

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,22 +4,14 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.RunCommand;
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-
-// Requires swerve migration to new Pose2D
-//import edu.wpi.first.math.geometry.Pose2d;
-
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
-
 import frc.robot.Constants.DriveConstants;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class RobotContainer {
-  private final static double kJoystickDeadband = 0.1;
+  private static final double kJoystickDeadband = 0.1;
 
   // The robot's subsystems and commands are defined here...
   private final DriveSubsystem driveSubsystem;
@@ -28,27 +20,31 @@ public class RobotContainer {
   public RobotContainer() {
     driveSubsystem = new DriveSubsystem();
 
-    driveSubsystem.setDefaultCommand(new RunCommand(
-      () -> {
-        double vx = getLeftX() * -DriveConstants.kMaxSpeedMetersPerSecond;
-        double vy = getLeftY() * -DriveConstants.kMaxSpeedMetersPerSecond;
-        double omega = getRightY() * -DriveConstants.kMaxOmega;
-        driveSubsystem.drive(vx, vy, omega);
-      }, driveSubsystem));
+    driveSubsystem.setDefaultCommand(
+        new RunCommand(
+            () -> {
+              double vx = getLeftX() * -DriveConstants.kMaxSpeedMetersPerSecond;
+              double vy = getLeftY() * -DriveConstants.kMaxSpeedMetersPerSecond;
+              double omega = getRightY() * -DriveConstants.kMaxOmega;
+              driveSubsystem.drive(vx, vy, omega);
+            },
+            driveSubsystem));
 
     configureButtonBindings();
   }
 
   private void configureButtonBindings() {
-    new JoystickButton(joystick, InterlinkButton.RESET.id).whenPressed(driveSubsystem::resetGyro, driveSubsystem);
+    new JoystickButton(joystick, InterlinkButton.RESET.id)
+        .whenPressed(driveSubsystem::resetGyro, driveSubsystem);
     // Requires swerve migration to new Pose2D
-    //new JoystickButton(joystick, InterlinkButton.HAMBURGER.id).whenPressed(() -> {driveSubsystem.resetOdometry(new Pose2d());},driveSubsystem);
+    // new JoystickButton(joystick, InterlinkButton.HAMBURGER.id).whenPressed(() ->
+    // {driveSubsystem.resetOdometry(new Pose2d());},driveSubsystem);
   }
 
-  //public Command getAutonomousCommand() {}
+  // public Command getAutonomousCommand() {}
 
-  //Interlink Controller Mapping
-  public enum Axis{
+  // Interlink Controller Mapping
+  public enum Axis {
     RIGHT_X(1),
     RIGHT_Y(0),
     LEFT_X(2),
@@ -58,32 +54,35 @@ public class RobotContainer {
     RIGHT_BACK(3);
 
     private final int id;
-    Axis(int id){
+
+    Axis(int id) {
       this.id = id;
     }
   }
 
-  public enum Shoulder{
+  public enum Shoulder {
     RIGHT_DOWN(2),
     LEFT_DOWN(4),
     LEFT_UP(5);
 
     private final int id;
-    Shoulder(int id){
+
+    Shoulder(int id) {
       this.id = id;
     }
   }
 
-  public enum Toggle{
+  public enum Toggle {
     LEFT_TOGGLE(1);
-    
+
     private final int id;
-    Toggle(int id){
+
+    Toggle(int id) {
       this.id = id;
     }
   }
 
-  public enum InterlinkButton{
+  public enum InterlinkButton {
     RESET(3),
     HAMBURGER(14),
     X(15),
@@ -91,12 +90,13 @@ public class RobotContainer {
     DOWN(17);
 
     private final int id;
-    InterlinkButton(int id){
+
+    InterlinkButton(int id) {
       this.id = id;
     }
   }
 
-  public enum Trim{
+  public enum Trim {
     LEFT_Y_POS(7),
     LEFT_Y_NEG(6),
     LEFT_X_POS(8),
@@ -107,26 +107,27 @@ public class RobotContainer {
     RIGHT_Y_NEG(13);
 
     private final int id;
-    Trim(int id){
+
+    Trim(int id) {
       this.id = id;
     }
   }
 
-  public double getLeftX(){
+  public double getLeftX() {
     double val = joystick.getRawAxis(Axis.LEFT_X.id);
-    if(Math.abs(val) < kJoystickDeadband) return 0.0;
+    if (Math.abs(val) < kJoystickDeadband) return 0.0;
     return val;
   }
 
-  public double getLeftY(){
+  public double getLeftY() {
     double val = joystick.getRawAxis(Axis.LEFT_Y.id);
-    if(Math.abs(val) < kJoystickDeadband) return 0.0;
+    if (Math.abs(val) < kJoystickDeadband) return 0.0;
     return val;
   }
 
-  public double getRightY(){
+  public double getRightY() {
     double val = joystick.getRawAxis(Axis.RIGHT_Y.id);
-    if(Math.abs(val) < kJoystickDeadband) return 0.0;
+    if (Math.abs(val) < kJoystickDeadband) return 0.0;
     return val;
   }
 }
