75e3159d77d1530a307336bd2d9963b82c371530
noahBana <nbana007@gmail.com>
2023-01-12 20:33:50 -0500

    add generateTrajectory toml parsing function

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -2,6 +2,9 @@ package frc.robot.subsystems;
 
 import static frc.robot.Constants.kTalonConfigTimeout;
 
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
@@ -11,10 +14,18 @@ import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.math.trajectory.TrajectoryConfig;
+import edu.wpi.first.math.trajectory.TrajectoryGenerator;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 import frc.robot.Constants.DriveConstants;
+import net.consensys.cava.toml.Toml;
+import net.consensys.cava.toml.TomlArray;
+import net.consensys.cava.toml.TomlParseResult;
+import net.consensys.cava.toml.TomlTable;
+
 import org.strykeforce.swerve.SwerveDrive;
 import org.strykeforce.swerve.TalonSwerveModule;
 
@@ -59,6 +70,7 @@ public class DriveSubsystem extends SubsystemBase {
 
     swerveDrive = new SwerveDrive(swerveModules);
     swerveDrive.resetGyro();
+    swerveDrive.setGyroOffset(Rotation2d.fromDegrees(0));
 
     // Setup Holonomic Controller
     ProfiledPIDController omegaCont =
@@ -105,11 +117,75 @@ public class DriveSubsystem extends SubsystemBase {
   //     swerveDrive.resetOdometry(pose);
   //     logger.info("reset odometry with: {}", pose);
   // }
+  public Rotation2d getGyroRotation2d() {
+    return swerveDrive.getHeading();
+  }
+    // Trajectory TOML Parsing
+  public PathData generateTrajectory(String trajectoryName) {
+
+    try {
+      TomlParseResult parseResult =
+          Toml.parse(Paths.get("/home/lvuser/deploy/paths/" + trajectoryName + ".toml"));
+      //logger.info("Generating Trajectory: {}", trajectoryName);
+      Pose2d startPose = parsePose2d(parseResult, "start_pose");
+      Pose2d endPose = parsePose2d(parseResult, "end_pose");
+      TomlArray internalPointsToml = parseResult.getArray("internal_points");
+      ArrayList<Translation2d> path = new ArrayList<>();
+      //logger.info("Toml Internal Points Array Size: {}", internalPointsToml.size());
+      
+      // Create a table for each internal point and put them into a translation2d waypoint 
+      for (int i = 0; i < internalPointsToml.size(); i++) {
+        TomlTable waypointToml = internalPointsToml.getTable(i);
+        Translation2d waypoint =
+            new Translation2d(waypointToml.getDouble("x"), waypointToml.getDouble("y"));
+        path.add(waypoint);
+      }
+
+      // Trajectory Config parsed from toml - any additional constraints would be added here
+      
+      TrajectoryConfig trajectoryConfig =
+          new TrajectoryConfig(
+              parseResult.getDouble("max_velocity"), parseResult.getDouble("max_acceleration"));
+      trajectoryConfig.setReversed(parseResult.getBoolean("is_reversed"));
+      trajectoryConfig.setStartVelocity(parseResult.getDouble("start_velocity"));
+      trajectoryConfig.setEndVelocity(parseResult.getDouble("end_velocity"));
+
+      //Yaw degrees is seperate from the trajectoryConfig 
+      double yawDegrees = parseResult.getDouble("target_yaw");
+      Rotation2d targetYaw = Rotation2d.fromDegrees(yawDegrees);
+      //logger.info("Yaw is {}", targetYaw);
+
+      //Create a the generated trajectory and return it along with the target yaw
+      Trajectory trajectoryGenerated =
+          TrajectoryGenerator.generateTrajectory(startPose, path, endPose, trajectoryConfig);
+      return new PathData(targetYaw, trajectoryGenerated);
+    } catch (Exception error) {
+      //logger.error(error.toString());
+      //logger.error("Path {} not found - Running Default Path", trajectoryName);
+      
+      //In the case of an error, set the trajectory to default
+      Trajectory trajectoryGenerated =
+          TrajectoryGenerator.generateTrajectory(
+              DriveConstants.startPose2d,
+              DriveConstants.getDefaultInternalWaypoints(),
+              DriveConstants.endPose2d,
+              DriveConstants.getDefaultTrajectoryConfig());
+
+      return new PathData(getGyroRotation2d(), trajectoryGenerated);
+    }
+  }
+
+  private Pose2d parsePose2d(TomlParseResult parseResult, String pose) {
+    return new Pose2d(
+        parseResult.getTable(pose).getDouble("x"),
+        parseResult.getTable(pose).getDouble("y"),
+        Rotation2d.fromDegrees(parseResult.getTable(pose).getDouble("angle")));
+  }
 
   public Pose2d getPoseMeters() {
     return swerveDrive.getPoseMeters();
   }
-
+  
   // Holonomic Controller
   // public void calculateController(State desiredState, Rotation2d desiredAngle){
   //     holoContInput = desiredState;
@@ -120,5 +196,7 @@ public class DriveSubsystem extends SubsystemBase {
   // holoContOutput.omegaRadiansPerSecond, false);
 
   // }
+
+
 
 }
