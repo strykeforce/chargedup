008b333c192f65ec56536a4b0006221b61e96986
David <dshen1248@gmail.com>
2023-01-13 19:18:20 -0500

    Added shoulder commands, pit dashboard

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,11 +4,23 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.commands.ShoulderSpeedCommand;
+import frc.robot.commands.ShoulderToPositionCommand;
+import frc.robot.subsystems.ShoulderSubsystem;
 
 public class RobotContainer {
+  private ShoulderSubsystem shoulderSubsystem;
+
   public RobotContainer() {
+    shoulderSubsystem = new ShoulderSubsystem();
+
+    configurePitDashboard();
     configureBindings();
   }
 
@@ -16,5 +28,25 @@ public class RobotContainer {
 
   public Command getAutonomousCommand() {
     return Commands.print("No autonomous command configured");
+  }
+
+  private void configurePitDashboard() {
+    ShuffleboardTab pitTab = Shuffleboard.getTab("Pit");
+
+    // Shoulder Commands
+    ShuffleboardLayout shoulderCommands =
+        pitTab.getLayout("Shoulder", BuiltInLayouts.kGrid).withPosition(0, 0).withSize(1, 3);
+    shoulderCommands
+        .add("Shoulder Stop", new ShoulderSpeedCommand(shoulderSubsystem, 0))
+        .withPosition(0, 0);
+    shoulderCommands
+        .add("Shoulder Zero", new ShoulderToPositionCommand(shoulderSubsystem, 0))
+        .withPosition(0, 1);
+    shoulderCommands
+        .add("Shoulder Forward", new ShoulderSpeedCommand(shoulderSubsystem, 0.1))
+        .withPosition(0, 2);
+    shoulderCommands
+        .add("Shoulder Reverse", new ShoulderSpeedCommand(shoulderSubsystem, -0.1))
+        .withPosition(0, 3);
   }
 }
--- src/main/java/frc/robot/subsystems/ShoulderSubsystem.java
+++ src/main/java/frc/robot/subsystems/ShoulderSubsystem.java
@@ -1,51 +1,81 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.DigitalInput;
+import frc.robot.Constants;
+import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.strykeforce.telemetry.TelemetryService;
+import org.strykeforce.telemetry.measurable.MeasurableSubsystem;
+import org.strykeforce.telemetry.measurable.Measure;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+public class ShoulderSubsystem extends MeasurableSubsystem {
+  private TalonSRX shoulderTalon;
+  private DigitalInput zeroShoulderInput = new DigitalInput(Constants.ShoulderConstants.kZeroId);
+  private double desiredPosition;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+  private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-public class ShoulderSubsystem extends SubsystemBase {
-    private TalonFX shoulderTalon;
-    private DigitalInput zeroShoulderInput = new DigitalInput(Constants.ShoulderConstants.kZeroId);
+  public ShoulderSubsystem() {
+    shoulderTalon = new TalonSRX(Constants.ShoulderConstants.kShoulderId);
+    shoulderTalon.configAllSettings(Constants.ShoulderConstants.getShoulderTalonConfig());
+    shoulderTalon.configSupplyCurrentLimit(
+        Constants.ShoulderConstants.getShoulderTalonSupplyLimitConfig());
 
-    private final Logger logger = LoggerFactory.getLogger(this.getClass());
+    desiredPosition = getPos();
+  }
 
-    public ShoulderSubsystem() {
-        shoulderTalon = new TalonFX(Constants.ShoulderConstants.kShoulderId);
-        shoulderTalon.configAllSettings(Constants.ShoulderConstants.getShoulderTalonConfig());
+  public void setPos(double location) {
+    desiredPosition = location;
+    shoulderTalon.set(ControlMode.MotionMagic, location);
+  }
+
+  public void setPct(double pct) {
+    shoulderTalon.set(ControlMode.PercentOutput, pct);
+  }
+
+  public void setDegs(double degs) {
+    setPos(Constants.ShoulderConstants.kZeroRads + degs * Constants.ShoulderConstants.kTicksPerDeg);
+  }
+
+  public double getPos() {
+    return shoulderTalon.getSelectedSensorPosition();
+  }
+
+  public boolean isFinished() {
+    return Math.abs(desiredPosition - getPos()) <= Constants.ShoulderConstants.kAllowedError;
+  }
+
+  public void zeroShoulder() {
+    if (zeroShoulderInput.get()) {
+      double absolute = shoulderTalon.getSelectedSensorPosition();
+      double offset = absolute - Constants.ShoulderConstants.kShoulderZeroTicks;
+
+      shoulderTalon.setSelectedSensorPosition(offset);
+
+      logger.info(
+          "Shoulder zeroed; offset: {} zeroTicks: {} absolute: {}",
+          offset,
+          Constants.ShoulderConstants.kShoulderZeroTicks,
+          absolute);
+
+    } else {
+      shoulderTalon.configPeakOutputForward(0, 0);
+      shoulderTalon.configPeakOutputReverse(0, 0);
+      logger.error("Shoulder zero failed. Killing shoulder...");
     }
+  }
 
-    public void setPos(double location) {
-        shoulderTalon.set(ControlMode.Position, location);
-    }
+  @Override
+  public void registerWith(TelemetryService telemetryService) {
+    super.registerWith(telemetryService);
+    telemetryService.register(shoulderTalon);
+  }
 
-    public void setPct(double pct) {
-        shoulderTalon.set(ControlMode.PercentOutput, pct);
-    }
-
-    public void zeroShoulder() {
-        if (zeroShoulderInput.get()) {
-            double absolute = shoulderTalon.getSensorCollection().getIntegratedSensorAbsolutePosition();
-            double offset = absolute - Constants.ShoulderConstants.kShoulderZeroTicks;
-
-            shoulderTalon.setSelectedSensorPosition(offset);
-
-            logger.info(
-                    "Shoulder zeroed; offset: {} zeroTicks: {} absolute: {}",
-                    offset,
-                    Constants.ShoulderConstants.kShoulderZeroTicks,
-                    absolute);
-
-        } else {
-            shoulderTalon.configPeakOutputForward(0, 0);
-            shoulderTalon.configPeakOutputReverse(0, 0);
-            logger.error("Shoulder zero failed. Killing shoulder...");
-        }
-    }
+  @Override
+  public Set<Measure> getMeasures() {
+    return Set.of();
+  }
 }
