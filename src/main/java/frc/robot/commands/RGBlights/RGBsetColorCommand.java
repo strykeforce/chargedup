94428d996784345ac7263e122307f73b563253b1
PotatoBoyH4 <isaac.hoekstra@gmail.com>
2023-01-30 20:10:52 -0500

    added a RGBlightsSubsystem for human player signaling. it includes an rgb set feature and a piece color set feature.

--- src/main/java/frc/robot/commands/RGBlights/RGBsetColorCommand.java
+++ src/main/java/frc/robot/commands/RGBlights/RGBsetColorCommand.java
@@ -3,11 +3,11 @@ package frc.robot.commands.RGBlights;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.subsystems.RGBlightsSubsystem;
 
-public class RGBsetColorCommand extends InstantCommand{
-    private RGBlightsSubsystem rgblightsSubsystem;
+public class RGBsetColorCommand extends InstantCommand {
+  private RGBlightsSubsystem rgblightsSubsystem;
 
-    public RGBsetColorCommand(RGBlightsSubsystem rgblightsSubsystem, int red, int green, int blue) {
-        this.rgblightsSubsystem = rgblightsSubsystem;
-        this.rgblightsSubsystem.setColor(red, green, blue);
-    }
+  public RGBsetColorCommand(RGBlightsSubsystem rgblightsSubsystem, int red, int green, int blue) {
+    this.rgblightsSubsystem = rgblightsSubsystem;
+    this.rgblightsSubsystem.setColor(red, green, blue);
+  }
 }
--- src/main/java/frc/robot/commands/RGBlights/RGBsetPieceCommand.java
+++ src/main/java/frc/robot/commands/RGBlights/RGBsetPieceCommand.java
@@ -1,25 +1,26 @@
 package frc.robot.commands.RGBlights;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import frc.robot.subsystems.RGBlightsSubsystem;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import edu.wpi.first.wpilibj2.command.InstantCommand;
-import frc.robot.subsystems.RGBlightsSubsystem;
+public class RGBsetPieceCommand extends InstantCommand {
+  private RGBlightsSubsystem rgblightsSubsystem;
+  private Logger logger = LoggerFactory.getLogger(RGBlightsSubsystem.class);
 
-public class RGBsetPieceCommand extends InstantCommand{
-    private RGBlightsSubsystem rgblightsSubsystem;
-    private Logger logger = LoggerFactory.getLogger(RGBlightsSubsystem.class);
-
-    public RGBsetPieceCommand(RGBlightsSubsystem rgblightsSubsystem, String CubeOrCone) {
-        this.rgblightsSubsystem = rgblightsSubsystem;
-        if (CubeOrCone == "Cube") {
-            this.rgblightsSubsystem.setCubeColor();
-            logger.info("set color to the Color of the {}", CubeOrCone);
-        } if (CubeOrCone == "Cone") {
-            this.rgblightsSubsystem.setConeColor();
-            logger.info("set color to the Color of the {}", CubeOrCone);
-        } else {
-            logger.info("failed to set Color to the color of the \"{}\"". reason: invalid argument.", CubeOrCone);
-        }
+  public RGBsetPieceCommand(RGBlightsSubsystem rgblightsSubsystem, String CubeOrCone) {
+    this.rgblightsSubsystem = rgblightsSubsystem;
+    if (CubeOrCone == "Cube") {
+      this.rgblightsSubsystem.setCubeColor();
+      logger.info("set color to the Color of the {}", CubeOrCone);
     }
+    if (CubeOrCone == "Cone") {
+      this.rgblightsSubsystem.setConeColor();
+      logger.info("set color to the Color of the {}", CubeOrCone);
+    } else {
+      logger.info(
+          "failed to set Color to the color of the \"{}\". reason: invalid argument.", CubeOrCone);
+    }
+  }
 }
