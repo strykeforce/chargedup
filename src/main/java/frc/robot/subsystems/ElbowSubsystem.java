daf545bb20dbeb04c74e1ca5a5dceedd0bc398b9
IRoot <74614800+imroot07@users.noreply.github.com>
2023-02-09 19:06:37 -0500

    updated elbow to use different encoder setup

--- src/main/java/frc/robot/subsystems/ElbowSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElbowSubsystem.java
@@ -27,10 +27,6 @@ public class ElbowSubsystem extends MeasurableSubsystem {
 
     remoteEncoder = new CANifier(ElbowConstants.kRemoteEncoderID);
 
-    elbowFalcon.configSelectedFeedbackSensor(RemoteFeedbackDevice.RemoteSensor0);
-    elbowFalcon.configRemoteFeedbackFilter(
-        ElbowConstants.kRemoteEncoderID, RemoteSensorSource.CANifier_Quadrature, 0);
-
     elbowFalcon.configForwardSoftLimitThreshold(ElbowConstants.kForwardSoftLimit);
     elbowFalcon.configForwardSoftLimitEnable(true);
     elbowFalcon.configReverseSoftLimitThreshold(ElbowConstants.kReverseSoftLimit);
@@ -48,7 +44,7 @@ public class ElbowSubsystem extends MeasurableSubsystem {
   private void zeroElbow() {
     int absoluteTicks = getPulseWidthFor(PWMChannel.PWMChannel0);
     int offset = absoluteTicks - ElbowConstants.kZeroTicks;
-    // elbowFalcon.setSelectedSensorPosition(offset);
+    elbowFalcon.setSelectedSensorPosition(offset);
     remoteEncoder.setQuadraturePosition(offset, 10);
     logger.info(
         "Zeroed elbow, absolute: {}, offset: {}, zero ticks: {}",
@@ -65,6 +61,11 @@ public class ElbowSubsystem extends MeasurableSubsystem {
   public void rotateClosedLoop(int posTicks) {
     elbowFalcon.set(ControlMode.MotionMagic, posTicks);
     setPointTicks = posTicks;
+  }
+
+  public double getRelativeDegs() {
+    return ElbowConstants.kZeroDegs
+        + getPulseWidthFor(PWMChannel.PWMChannel0) / ElbowConstants.kTicksPerDeg;
   }
 
   public boolean isElbowAtPos() {
