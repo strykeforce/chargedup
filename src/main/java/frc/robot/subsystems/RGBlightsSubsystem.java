a07d80fffda1ca1a3b239ca67f33f204d9642f5b
PotatoBoyH4 <isaac.hoekstra@gmail.com>
2023-02-23 20:13:55 -0500

    kept fixing bugs. one wierd bug went away on its own. will watch for it in future

--- src/main/java/frc/robot/subsystems/RGBlightsSubsystem.java
+++ src/main/java/frc/robot/subsystems/RGBlightsSubsystem.java
@@ -1,8 +1,8 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DigitalOutput;
-import edu.wpi.first.wpilibj.PWM;
-import edu.wpi.first.wpilibj.PWM.PeriodMultiplier;
+// import edu.wpi.first.wpilibj.PWM;
+// import edu.wpi.first.wpilibj.PWM.PeriodMultiplier;
 import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +10,6 @@ import org.strykeforce.telemetry.measurable.MeasurableSubsystem;
 import org.strykeforce.telemetry.measurable.Measure;
 
 public class RGBlightsSubsystem extends MeasurableSubsystem {
-  private PWM relay;
   // private PWM red_channel;
   // private PWM green_channel;
   // private PWM blue_channel;
@@ -26,11 +25,9 @@ public class RGBlightsSubsystem extends MeasurableSubsystem {
   }
 
   public RGBlightsSubsystem() {
-    relay = new PWM(0);
     // red_channel = new PWM(0);
     // green_channel = new PWM(1);
     // blue_channel = new PWM(2);
-    relay.setPeriodMultiplier(PeriodMultiplier.k1X);
     // red_channel.setPeriodMultiplier(PeriodMultiplier.k1X);
     // green_channel.setPeriodMultiplier(PeriodMultiplier.k1X);
     // blue_channel.setPeriodMultiplier(PeriodMultiplier.k1X);
@@ -46,38 +43,38 @@ public class RGBlightsSubsystem extends MeasurableSubsystem {
   }
 
   public void setColor(Double red, Double green, Double blue) {
-    double R = red * 5000 - 500;
-    if (R < 0) {
-      R = 0;
-    } else if (R > 4000) {
-      R = 4000;
-    }
+    // double R = red * 5000 - 500;
+    // if (R < 0) {
+    //   R = 0;
+    // } else if (R > 4000) {
+    //   R = 4000;
+    // }
 
-    double G = green * 5000 - 500;
-    if (G < 0) {
-      G = 0;
-    } else if (G > 4000) {
-      G = 4000;
-    }
+    // double G = green * 5000 - 500;
+    // if (G < 0) {
+    //   G = 0;
+    // } else if (G > 4000) {
+    //   G = 4000;
+    // }
 
-    double B = blue * 5000 - 500;
-    if (B < 0) {
-      B = 0;
-    } else if (B > 4000) {
-      B = 4000;
-    }
+    // double B = blue * 5000 - 500;
+    // if (B < 0) {
+    //   B = 0;
+    // } else if (B > 4000) {
+    //   B = 4000;
+    // }
 
     // red_channel.setRaw((int) R);
     // green_channel.setRaw((int) G);
     // blue_channel.setRaw((int) B);
-    logger.info("set color to R {} G {} B {}", R, G, B);
+
+    // logger.info("set color to R {} G {} B {}", R, G, B);
     red_channel_D.updateDutyCycle(red);
     green_channel_D.updateDutyCycle(green);
     blue_channel_D.updateDutyCycle(blue);
   }
 
   public void setCubeColor() {
-    relay.setRaw(4095);
     this.setColor(0.43, 0.01, 0.43);
   }
 
