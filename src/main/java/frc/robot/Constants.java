45eac4ea4912492aa2a639fd0ded865d0465f3ed
kennyseybold <42521401+kennyseybold@users.noreply.github.com>
2023-03-31 18:58:40 -0400

    slowing

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -441,7 +441,7 @@ public class Constants {
       elevatorConfig.slot0.integralZone = 0;
       elevatorConfig.slot0.maxIntegralAccumulator = 0;
       elevatorConfig.slot0.allowableClosedloopError = 0;
-      elevatorConfig.motionCruiseVelocity = 14_000; // 10_000
+      elevatorConfig.motionCruiseVelocity = 12_000; // 10_000
       elevatorConfig.motionAcceleration = 200_000; // 100_000
 
       elevatorConfig.forwardSoftLimitEnable = true;
